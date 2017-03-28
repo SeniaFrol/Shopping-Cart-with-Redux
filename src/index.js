8f6f27504b89adf032125758869a126dff17e439
Senia <senia.frolov@gmail.com>
2017-03-28 10:31:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './containers/app.js';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import App from './containers/app';
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer);
